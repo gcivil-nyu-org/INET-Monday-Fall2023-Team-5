81262615577d2f0402b8a5dda0ecc9218affd26b
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-14 01:03:43 -0400

    Update views.py

--- roleplaydate/views.py
+++ roleplaydate/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
 
 def mockup_signup(request):
     print("Mockup signup view visited!")
     return HttpResponseRedirect(reverse('login'))
+
+def menu(request):
+    return render(request,"menu.html")
+
+def profile(request):
+    return render(request,"profile.html")
