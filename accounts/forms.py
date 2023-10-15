623af60cfe44936d754e3744acd16922fc14ad42
aya0221 <ao2368@nyu.edu>
2023-10-15 12:00:22 -0400

    added Pronoun Preferences setting features

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from .models import Profile
 
 class EditProfileForm(forms.ModelForm):
+     pronoun_preference = forms.ChoiceField(
+        choices=[
+            ('he_him', 'He/Him'),
+            ('she_her', 'She/Her'),
+            ('they_them', 'They/Them'),
+            ('other', 'Other'),
+        ],
+        widget=forms.RadioSelect,
+        required=False
+    )
+     custom_pronoun = forms.CharField(
+          max_length=255,
+          required=False,
+          widget=forms.TextInput(attrs={'placeholder': 'Custom Pronoun'}),
+     )
      
      class Meta:
           model = Profile
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,31 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import EditProfileForm
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+def edit_profile(request):
+    profile = request.user.profile  # Assuming your Profile model is related to the User model
+
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            # Update the user's profile data here based on the form data
+            profile.open_to_dating = form.cleaned_data['open_to_dating']
+            pronoun_preference = form.cleaned_data['pronoun_preference']
+            if pronoun_preference == 'other':
+                profile.custom_pronoun = form.cleaned_data['custom_pronoun']
+            else:
+                profile.custom_pronoun = None
+            profile.save()
+            return redirect('profile')  # Redirect to the user's profile page
+    else:
+        form = EditProfileForm(instance=profile)
+
+    return render(request, 'edit_profile.html', {'form': form})
