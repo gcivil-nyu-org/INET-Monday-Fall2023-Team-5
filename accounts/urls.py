f1dfe44c388505ff6b346ad251c59e4ac03224c9
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-18 19:12:20 -0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("edit_profile/", edit_profile, name="edit_profile"),
     path("profile_updated/", profile_updated, name="profile_updated"),
+    path("profile/", view_profile, name="view_profile"),
+    path('browse/', browse_profiles, name='browse_profiles'),
 ]
