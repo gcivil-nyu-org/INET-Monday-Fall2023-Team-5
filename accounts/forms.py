f460581b1c2e378793c657da3ac87b45bfeffd53
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-19 09:31:29 -0400

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,29 +2,40 @@ from django import forms
 from .models import*
 
 class EditProfileForm(forms.ModelForm):
-     pronoun_preference = forms.ChoiceField(
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('N', 'Non-binary'),
+        ('NS', 'Not Specified')
+    ]
+
+    pronoun_preference = forms.ChoiceField(
         choices=[
             ('he_him', 'He/Him'),
             ('she_her', 'She/Her'),
             ('they_them', 'They/Them'),
             ('other', 'Other'),
+            ('not_specified', 'Not Specified'),  # Added this choice
         ],
         widget=forms.RadioSelect,
         required=False
     )
-     open_to_dating = forms.ModelMultipleChoiceField(
+
+    open_to_dating = forms.ModelMultipleChoiceField(
         queryset=DatingPreference.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
-     custom_pronoun = forms.CharField(
-          max_length=255,
-          required=False,
-          widget=forms.TextInput(attrs={'placeholder': 'Custom Pronoun'}),
-     )
 
-     class Meta:
-          model = Profile
-          fields = ('gender','open_to_dating','pronoun_preference')
+    custom_pronoun = forms.CharField(
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Custom Pronoun'}),
+    )
+
+    class Meta:
+        model = Profile
+        fields = ('gender', 'open_to_dating', 'pronoun_preference', 'custom_pronoun')  # Added 'custom_pronoun'
+
 
     
