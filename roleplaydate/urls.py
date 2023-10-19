6a41c5a5405c3c1ff2b2ac7c00225da4452d0e59
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-19 08:53:20 -0400

    Update urls.py

--- roleplaydate/urls.py
+++ roleplaydate/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
