a51731eaee71858ba4f6ec830cfa68b53f3e518c
Qi Peng <qp2039@nyu.edu>
2023-10-19 20:14:58 -0400

    temp update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,32 @@
 from django import forms
 from .models import Profile
+from django.contrib.auth.models import User
 
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+
+
+# This is the form that will be used to create a new user, and check for valid email
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="NYU Email")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("@nyu.edu"):
+            raise ValidationError("Please use your NYU email.")
+        return email
+
+    
+# This is the form that will be used to edit a user's profile
 class EditProfileForm(forms.ModelForm):
      pronoun_preference = forms.ChoiceField(
         choices=[
@@ -22,4 +48,3 @@ class EditProfileForm(forms.ModelForm):
           model = Profile
           fields = ('open_to_dating','pronoun_preference')
 
-    
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
     gender_choices_pref = (('Males', 'Males'),('Females', 'Females'), ('Non-binary Individuals', 'Non-binary Individuals'))
     open_to_dating = models.CharField(max_length = 25, choices = gender_choices_pref, blank = False)
 
