0c171ab823b1528ff142cff4d2679cbebc3ff325
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-25 11:09:42 -0400

    Added ProfileModelTest
    -setUp
    -test_create_profile
    -test_string_representation
    -test_default_values
    -test_profile_picture_field
    -test_automatic_profile_creation_with_user
    -test_profile_deletion_with_user

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,25 +4,76 @@ from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.auth.forms import PasswordChangeForm
+from .models import *
+
+class ProfileModelTest(TestCase):
+    
+    def setUp(self):
+        # Create a test user for associating with a profile
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+    
+    def test_create_profile(self):
+        # Given the post-save signal, there should already be one profile.
+        # We will just verify that the profile was created correctly.
+        profile = self.user.profile
+        self.assertEqual(Profile.objects.count(), 1)
+        self.assertEqual(profile.user, self.user)
+        
+    def test_string_representation(self):
+        # Test the string representation of the profile.
+        profile = self.user.profile
+        self.assertEqual(str(profile), 'testuser')
+
+    def test_default_values(self):
+        # Test the default values for fields.
+        profile = self.user.profile
+        self.assertEqual(profile.gender, 'NS')
+        self.assertEqual(profile.pronoun_preference, 'not_specified')
+
+    def test_profile_picture_field(self):
+        # Retrieve the existing profile associated with the user.
+        profile = self.user.profile
+    
+        # Check that the profile_picture field can be left blank.
+        self.assertFalse(profile.profile_picture.name)
+
+
+    def test_automatic_profile_creation_with_user(self):
+        # Given that only one user has been created in the setUp,
+        # there should already be one profile associated with that user.
+        self.assertEqual(Profile.objects.count(), 1)
+        self.assertEqual(self.user.profile.user, self.user)
+
+    def test_profile_deletion_with_user(self):
+        self.user.delete()
+        self.assertEqual(Profile.objects.count(), 0)
+
 
 class AccountViewTest(TestCase):
     
     def setUp(self):
+        # Create a test user for authentication and testing purposes
         self.client = Client()
         self.account_url = reverse('account')  # assuming 'account' is the name of the URL pattern for the account view
         self.user = User.objects.create_user(username='testuser', password='testpassword123')
 
     def test_redirect_if_not_logged_in(self):
+        # Tests if an unauthenticated  user trying to access the account
+        #  view is redirected to the login page
         response = self.client.get(self.account_url)
         self.assertRedirects(response, '/accounts/login/?next=/accounts/account/') # adjust the expected URL if your login URL is different
 
     def test_logged_in_uses_correct_template(self):
+        # Test that if a user is logged in and accesses the account 
+        # view, the correct template (account.html) is used
         self.client.login(username='testuser', password='testpassword123')
         response = self.client.get(self.account_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account.html')
 
     def test_change_username_successfully(self):
+        # Test checks if a logged-in user can successfully change 
+        # their username
         self.client.login(username='testuser', password='testpassword123')
         response = self.client.post(self.account_url, {'username': 'newusername'})
         self.user.refresh_from_db()
@@ -32,6 +83,8 @@ class AccountViewTest(TestCase):
         self.assertEqual(str(messages[0]), 'Username updated successfully')
 
     def test_change_username_to_existing_one(self):
+        # Test that a logged-in user cannot change their username 
+        # to one that already exists in the database
         User.objects.create_user(username='existinguser', password='testpassword123')
         self.client.login(username='testuser', password='testpassword123')
         response = self.client.post(self.account_url, {'username': 'existinguser'})
@@ -40,6 +93,8 @@ class AccountViewTest(TestCase):
         self.assertEqual(str(messages[0]), 'This username is already taken. Choose another.')
 
     def test_change_password_successfully(self):
+        # Test checks if a logged-in user can successfully change 
+        # their password.
         self.client.login(username='testuser', password='testpassword123')
         data = {
             'old_password': 'testpassword123',
@@ -52,6 +107,3 @@ class AccountViewTest(TestCase):
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Password updated successfully')
-
-   
-
