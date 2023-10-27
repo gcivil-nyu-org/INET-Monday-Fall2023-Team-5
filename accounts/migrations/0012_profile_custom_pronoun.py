34c80021ed16e3b8e50d1631dcf98498c9696d63
aya0221 <ao2368@nyu.edu>
2023-10-27 13:09:08 -0400

    Refined accounts app URL routing and ensured consistency with LOGIN and LOGOUT redirects. - Removed redundant base URL route in accounts/urls.py.

--- accounts/migrations/0012_profile_custom_pronoun.py
+++ accounts/migrations/0012_profile_custom_pronoun.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('accounts', '0011_profile_profile_picture'),
+        ("accounts", "0011_profile_profile_picture"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='profile',
-            name='custom_pronoun',
+            model_name="profile",
+            name="custom_pronoun",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth.views import LoginView  # override the default login view
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.contrib.auth.views import (
     PasswordResetView,
     PasswordResetDoneView,
@@ -10,6 +10,7 @@ from django.contrib.auth.views import (
 )
 
 urlpatterns = [
+    # path("", home, name="home"),
     path("about/", about, name="about"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("edit_profile/", edit_profile, name="edit_profile"),
@@ -26,6 +27,7 @@ urlpatterns = [
         LoginView.as_view(template_name="accounts/registration/login.html"),
         name="login",
     ),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path(
         "password_reset/",
         PasswordResetView.as_view(
