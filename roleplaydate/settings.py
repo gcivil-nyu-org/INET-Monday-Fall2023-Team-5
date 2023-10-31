5f8ab333370d85e3f17c3f2c089f724f57ccc789
Andres Zarta <andreszarta@andress-mbp-2.mynetworksettings.com>
2023-10-31 01:13:05 -0400

    Environ back to exception raising

--- roleplaydate/settings.py
+++ roleplaydate/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "roleplaydate.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("RDS_DB_NAME"),
-        "USER": os.environ.get("RDS_USERNAME"),
-        "PASSWORD": os.environ.get("RDS_PASSWORD"),
-        "HOST": os.environ.get("RDS_HOSTNAME"),
-        "PORT": os.environ.get("RDS_PORT"),
+        "NAME": os.environ["RDS_DB_NAME"],
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
         "OPTIONS": {
             "sslmode": "require",
         },
