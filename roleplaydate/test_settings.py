045334a7c91fab7a98c9b7d8cde8db29885db4ed
Andres Nicolas Zarta <83557600+AndresZarta@users.noreply.github.com>
2023-11-02 00:46:56 -0400

    Update test_settings.py

--- roleplaydate/test_settings.py
+++ roleplaydate/test_settings.py
@@ -1,15 +1,1 @@
 from .settings import *
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "default",
-        "USER": os.environ["RDS_USERNAME"],
-        "PASSWORD": os.environ["RDS_PASSWORD"],
-        "HOST": os.environ["RDS_HOSTNAME"],
-        "PORT": os.environ["RDS_PORT"],
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
-}
