2eda02041ae391d9290e5f0e4b737959ff73b891
Andres Nicolas Zarta <83557600+AndresZarta@users.noreply.github.com>
2023-11-02 10:01:10 -0400

    Update test_settings.py
    Testing database environment to develop in, this database is OK to break.

--- roleplaydate/test_settings.py
+++ roleplaydate/test_settings.py
@@ -1,1 +1,15 @@
 from .settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "develop_database",
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
+}
