345f9717bb8fd3bb2fc16bab078a656f48cc127d
Andres Nicolas Zarta <83557600+AndresZarta@users.noreply.github.com>
2023-11-02 00:28:50 -0400

    Update test_settings.py

--- roleplaydate/test_settings.py
+++ roleplaydate/test_settings.py
@@ -2,7 +2,14 @@ from .settings import *
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "default",
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
