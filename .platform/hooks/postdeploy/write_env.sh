903f34fe41e33d8de7d6f933ea2fb0f72ced3543
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-03 11:35:58 -0400

    Update write_env.sh

--- .platform/hooks/postdeploy/write_env.sh
+++ .platform/hooks/postdeploy/write_env.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# Save environment variables to a file
+LOG_FILE="/var/log/eb-hooks.log"
+
+echo "$(date): Executing write_env.sh" >> "$LOG_FILE"
+
+# Attempt to save environment variables to a file
 /opt/elasticbeanstalk/bin/get-config environment > /opt/elasticbeanstalk/deployment/envvars.json
+
+# Check the exit status of the previous command
+if [ $? -eq 0 ]; then
+    echo "$(date): Successfully wrote environment variables to /opt/elasticbeanstalk/deployment/envvars.json" >> "$LOG_FILE"
+else
+    echo "$(date): Failed to write environment variables" >> "$LOG_FILE"
+fi
