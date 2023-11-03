24d0c2a3728d95ac8dc0f5dce5d19b4d7a6400b1
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-02 22:19:47 -0400

    Update reset_likes.py
    Cron job now clears the likes table

--- accounts/management/commands/reset_likes.py
+++ accounts/management/commands/reset_likes.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
-from accounts.models import Profile
-
+from accounts.models import Profile, Like
 
 class Command(BaseCommand):
-    help = "Resets the number of likes for all users to 3"
+    help = "Resets the number of likes for all users to 3 and clears all likes"
 
     def handle(self, *args, **kwargs):
+        # Clear all the likes
+        Like.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS("Successfully cleared all likes"))
+
+        # Reset the like counters
         Profile.objects.update(likes_remaining=3)
         self.stdout.write(self.style.SUCCESS("Successfully reset likes for all users"))
