c24b8c57e38441a62906ae8ba3e53edb1ad39ad4
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-03 14:14:21 -0400

    Update reset_likes.py

--- accounts/management/commands/reset_likes.py
+++ accounts/management/commands/reset_likes.py
@@ -4,19 +4,11 @@ from accounts.models import Profile, Like
 class Command(BaseCommand):
     help = "Resets the number of likes for all users to 3 and clears all likes"
 
-    def add_arguments(self, parser):
-        parser.add_argument('--dbname', help='Database name', default='default')
-
-    def handle(self, *args, **options):
-        # If --dbname is provided, you can use it to connect to a specific database
-        db_name = options['dbname']
-        # Ensure your database operations target the 'db_name' database
-        # For example, Like.objects.using(db_name).all().delete()
-
+    def handle(self, *args, **kwargs):
         # Clear all the likes
-        Like.objects.using(db_name).all().delete()
+        Like.objects.all().delete()
         self.stdout.write(self.style.SUCCESS("Successfully cleared all likes"))
 
         # Reset the like counters
-        Profile.objects.using(db_name).update(likes_remaining=3)
+        Profile.objects.update(likes_remaining=3)
         self.stdout.write(self.style.SUCCESS("Successfully reset likes for all users"))
