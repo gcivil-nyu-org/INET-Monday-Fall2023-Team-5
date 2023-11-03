3a2ea82a247f372d61bbcc73272842f50e632e67
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-03 11:08:02 -0400

    Delete load_and_run.sh

--- load_and_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# load_and_run.sh
-
-# Load the environment variables
-source /opt/elasticbeanstalk/deployment/envvars.json
-
-# Run the Django management command
-cd /var/app/current && /var/app/venv/*/bin/python manage.py reset_likes
