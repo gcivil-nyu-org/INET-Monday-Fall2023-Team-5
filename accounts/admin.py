194c62551b9bf4950d423d12743290dbaef08b85
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-03 16:08:22 -0400

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, DatingPreference, Like
+from .models import Profile, DatingPreference, Like, Match
 
 
 @admin.register(Profile)
@@ -21,19 +21,29 @@ class ProfileAdmin(admin.ModelAdmin):
 admin.site.register(DatingPreference)
 
 
+@admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = (
-        "from_user",
-        "to_user",
-        "created_at",
-    )  # Fields to display in the admin list view
-    search_fields = (
-        "from_user__username",
-        "to_user__username",
-    )  # Fields to search in the admin
-    list_filter = ("created_at",)  # Fields to filter in the admin list view
-    ordering = ("-created_at",)  # Default ordering
+    list_display = ('from_user', 'to_user', 'created_at', 'is_mutual')
+    list_filter = ('from_user', 'to_user')
+    search_fields = ('from_user__username', 'to_user__username')
+    actions = ['make_mutual']
+
+    def make_mutual(self, request, queryset):
+        for like in queryset:
+            # Here you would implement the logic to check and create a mutual like
+            pass
+    make_mutual.short_description = "Mark selected likes as mutual"
+
+    def is_mutual(self, obj):
+        return obj.is_mutual()
+    is_mutual.boolean = True
+    is_mutual.short_description = 'Mutual Like'
 
 
 # Register the Like model with the admin site
 admin.site.register(Like, LikeAdmin)
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('user1', 'user2', 'matched_at')
+    search_fields = ('user1__username', 'user2__username')
