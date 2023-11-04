f61541e42ca855c31229023130bc7063d4750984
Aya <ao2368@nyu.edu>
2023-11-04 17:34:58 -0400

    Add liking feature to user profiles
    - Implemented a 'like' button on user profile pages
    - Added JavaScript to asynchronously update the like count
    - Ensured compatibility with existing user features

--- accounts/views.py
+++ accounts/views.py
@@ -10,12 +10,13 @@ from .models import Like, Profile
 from django.core.paginator import Paginator
 from django.contrib.auth.tokens import default_token_generator
 from .forms import CustomUserCreationForm
-from django.contrib.auth.models import User
+from .models import User, Like, Profile, Match
 from django.utils.http import urlsafe_base64_decode
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth import update_session_auth_hash
 from django.http import JsonResponse
+from django.db.models import Q
 
 
 class SignUpView(generic.CreateView):
@@ -249,43 +250,54 @@ def like_profile(request, user_id):
     if request.method == "POST":
         receiving_user = get_object_or_404(User, pk=user_id)
         current_user_profile = request.user.profile
-
-        # Check if the user has already liked this profile
-        existing_like = Like.objects.filter(
-            from_user=request.user, to_user=receiving_user
-        ).first()
+        existing_like = Like.objects.filter(from_user=request.user, to_user=receiving_user).first()
 
         if existing_like:
-            # Unlike the profile
-            existing_like.delete()
-            # Increment the likes_remaining.
-            current_user_profile.likes_remaining += 1
-            current_user_profile.save()
+            return JsonResponse({"success": False, "error": "You have already liked this user."})
+            # # Unlike the profile
+            # # existing_like.delete()
+            # # Increment the likes_remaining.
+            # current_user_profile.likes_remaining += 1
+            # current_user_profile.save()
 
-            return JsonResponse(
-                {
-                    "success": True,
-                    "likes_remaining": current_user_profile.likes_remaining,
-                    "action": "unliked",
-                }
-            )
+            # return JsonResponse(
+            #     {
+            #         "success": True,
+            #         "likes_remaining": current_user_profile.likes_remaining,
+            #         "action": "unliked",
+            #     }
+            # )
 
         # Check if the current user has likes remaining.
         elif current_user_profile.likes_remaining > 0:
             # Create a like.
             Like.objects.create(from_user=request.user, to_user=receiving_user)
-            # Decrement the likes_remaining.
             current_user_profile.likes_remaining -= 1
             current_user_profile.save()
+
+            # Check for mutual like
+            mutual_like = Like.objects.filter(from_user=receiving_user, to_user=request.user).exists()
+            # Check if either user is already matched
+            user_already_matched = Match.objects.filter(
+                Q(user1=request.user) | Q(user2=request.user) |
+                Q(user1=receiving_user) | Q(user2=receiving_user)
+            ).exists()
+
+            if mutual_like and not user_already_matched:
+                # Create a match
+                Match.objects.create(user1=request.user, user2=receiving_user)
+                # You can add any notification logic here if needed
 
             return JsonResponse(
                 {
                     "success": True,
                     "likes_remaining": current_user_profile.likes_remaining,
                     "action": "liked",
+                    "match_created": mutual_like and not user_already_matched
                 }
             )
 
-        return JsonResponse({"success": False, "error": "No likes remaining."})
+        else:
+            return JsonResponse({"success": False, "error": "No likes remaining."})
 
     return JsonResponse({"success": False, "error": "Invalid method."})
