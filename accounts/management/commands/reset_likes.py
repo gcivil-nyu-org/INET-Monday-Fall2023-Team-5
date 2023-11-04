2e6ad5c4208ff7ec9da16b66cfc0d399d4dd45a5
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-04 14:41:23 -0400

    Update reset_likes.py

--- accounts/management/commands/reset_likes.py
+++ accounts/management/commands/reset_likes.py
@@ -1,19 +1,22 @@
 from django.core.management.base import BaseCommand
 from accounts.models import Profile, Like
 
+
 class Command(BaseCommand):
     help = "Resets the number of likes for all users to 3 and clears all likes"
 
     def add_arguments(self, parser):
-        parser.add_argument('--dbname', type=str, help='Optional database name')
+        parser.add_argument("--dbname", type=str, help="Optional database name")
 
     def handle(self, *args, **options):
-        dbname = options.get('dbname')
+        dbname = options.get("dbname")
         if dbname:
             self.stdout.write(f"Using database: {dbname}")
             # Here you would add logic to switch to the right database if necessary
-            # For example, using Django's database routers or manually selecting the database
-            # using `using(dbname)` queryset method if your app handles multiple databases.
+            # For example, using Django's database routers or
+            # manually selecting the database
+            # using `using(dbname)` queryset method if
+            # your app handles multiple databases.
 
         # Clear all the likes
         Like.objects.all().delete()
