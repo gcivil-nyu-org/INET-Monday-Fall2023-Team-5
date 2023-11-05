62a0fe6f71e751ae9de2503ac5f882fa506098a8
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-04 20:08:04 -0400

    Update views.py

--- tags/views.py
+++ tags/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Tag
+
 # Create your views here
+
 
 def tag_view(request):
     words = Tag.objects.all()  # Fetch words from the database
-    context = {'words': words}
-    return render(request, 'tags/tag_to_sentence.html', context)
-
+    context = {"words": words}
+    return render(request, "tags/tag_to_sentence.html", context)
