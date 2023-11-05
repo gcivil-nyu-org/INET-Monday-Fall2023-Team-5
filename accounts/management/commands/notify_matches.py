202ff655b404988c78322f5da0c3ff1a441758d5
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-05 17:09:00 -0500

    Update notify_matches.py

--- accounts/management/commands/notify_matches.py
+++ accounts/management/commands/notify_matches.py
@@ -9,27 +9,30 @@ import logging
 # Configure a logger for the command
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-    help = 'Sends notifications to users about new matches at midnight.'
+    help = "Sends notifications to users about new matches at midnight."
 
     def handle(self, *args, **options):
         yesterday = timezone.now() - timedelta(days=1)
-        new_matches = Match.objects.filter(matched_at__gte=yesterday, notification_sent=False)
+        new_matches = Match.objects.filter(
+            matched_at__gte=yesterday, notification_sent=False
+        )
 
         for match in new_matches:
             try:
                 # Send notification to user1
                 send_mail(
-                    subject='You have a new match!',
-                    message=f'Hello {match.user1.username},\n\nYou have been matched with someone on our platform. Please log in to see more details about your match.',
+                    subject="You have a new match!",
+                    message=f"Hello {match.user1.username},\n\nYou have been matched with someone on our platform. Please log in to see more details about your match.",
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     recipient_list=[match.user1.email],
                     fail_silently=False,
                 )
                 # Send notification to user2
                 send_mail(
-                    subject='You have a new match!',
-                    message=f'Hello {match.user2.username},\n\nYou have been matched with someone on our platform. Please log in to see more details about your match.',
+                    subject="You have a new match!",
+                    message=f"Hello {match.user2.username},\n\nYou have been matched with someone on our platform. Please log in to see more details about your match.",
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     recipient_list=[match.user2.email],
                     fail_silently=False,
@@ -39,8 +42,18 @@ class Command(BaseCommand):
                 match.notification_sent = True
                 match.save()
 
-                self.stdout.write(self.style.SUCCESS(f'Notification sent for match between {match.user1.username} and {match.user2.username}'))
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Notification sent for match between {match.user1.username} and {match.user2.username}"
+                    )
+                )
 
             except Exception as e:
-                logger.error(f'Failed to send notification for match between {match.user1.username} and {match.user2.username}: {e}')
-                self.stderr.write(self.style.ERROR(f'Error sending notification for match between {match.user1.username} and {match.user2.username}: {e}'))
+                logger.error(
+                    f"Failed to send notification for match between {match.user1.username} and {match.user2.username}: {e}"
+                )
+                self.stderr.write(
+                    self.style.ERROR(
+                        f"Error sending notification for match between {match.user1.username} and {match.user2.username}: {e}"
+                    )
+                )
