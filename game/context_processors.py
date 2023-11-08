32663c973d89fb17c7583c8832f9b2f5c3cf087e
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-08 17:42:35 -0500

    Update context_processors.py

--- game/context_processors.py
+++ game/context_processors.py
@@ -1,19 +1,28 @@
 from game.models import GameSession
 import logging
+from django.db.models import Q
+
 
 logger = logging.getLogger(__name__)
 
 def game_session_processor(request):
     if request.user.is_authenticated:
         try:
+            # Filter GameSessions where the user is either playerA or playerB
             game_session = GameSession.objects.filter(
-                # Your conditions here
-            ).latest('id')
+                Q(playerA__user=request.user) | Q(playerB__user=request.user),
+                is_active=True  # Assuming you want only active game sessions
+            ).latest('id')  # Assuming 'id' is an auto-incrementing field which reflects the creation order
+
             game_session_url = game_session.get_absolute_url()
-            logger.info(f"Game session URL: {game_session_url}")
+            logger.info(f"Game session URL for user {request.user.username}: {game_session_url}")
             return {'game_session_url': game_session_url}
+
         except GameSession.DoesNotExist:
-            logger.info("No game session found")
+            logger.info(f"No game session found for user {request.user.username}")
             return {'game_session_url': None}
+
+    # Return an empty dictionary if the user is not authenticated
     return {}
+
 
