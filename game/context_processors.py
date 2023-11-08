ea3130a18c2d3008ce07636dc778555c5e595fce
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-08 17:15:44 -0500

    Update context_processors.py

--- game/context_processors.py
+++ game/context_processors.py
@@ -1,6 +1,6 @@
 from game.models import GameSession
 
-def game_session_processor(request):
+""" def game_session_processor(request):
     # Logic to get the most recent game session URL for the current user
     if request.user.is_authenticated:
         try:
@@ -12,4 +12,22 @@ def game_session_processor(request):
         except GameSession.DoesNotExist:
             # Handle the case where there is no game session
             return {'game_session_url': None}
+    return {} """
+
+import logging
+logger = logging.getLogger(__name__)
+
+def game_session_processor(request):
+    if request.user.is_authenticated:
+        try:
+            game_session = GameSession.objects.filter(
+                # Your conditions here
+            ).latest('id')
+            game_session_url = game_session.get_absolute_url()
+            logger.info(f"Game session URL: {game_session_url}")
+            return {'game_session_url': game_session_url}
+        except GameSession.DoesNotExist:
+            logger.info("No game session found")
+            return {'game_session_url': None}
     return {}
+
