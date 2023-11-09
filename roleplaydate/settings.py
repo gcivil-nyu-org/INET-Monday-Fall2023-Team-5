60375288fc0a3fb68b84d8f9b17d10c49e8fdb12
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-09 07:45:44 -0500

    formatting

--- roleplaydate/settings.py
+++ roleplaydate/settings.py
@@ -149,6 +149,3 @@ EMAIL_HOST_PASSWORD = os.environ.get("RPTHENDATE_EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Roleplay then Date roleplayanddate@gmail.com"
-
-
-
