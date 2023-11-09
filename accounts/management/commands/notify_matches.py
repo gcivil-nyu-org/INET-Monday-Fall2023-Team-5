11a57e190a74b2227014da804ddea62651138008
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-09 07:52:25 -0500

    formatting

--- accounts/management/commands/notify_matches.py
+++ accounts/management/commands/notify_matches.py
@@ -5,13 +5,12 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.db import transaction
 import logging
-
+from django.urls import reverse
 from accounts.models import Match
 from game.models import GameSession, Player
 
 # Configure a logger for the command
 logger = logging.getLogger(__name__)
-
 
 class Command(BaseCommand):
     help = "Sends notifications to users about new matches at midnight."
@@ -55,11 +54,17 @@ class Command(BaseCommand):
                 match.notification_sent = True
                 match.save()
 
-                success_message = f"Notification sent for match between {match.user1.username} and {match.user2.username}."
+                success_message = (
+                    f"Notification sent for match between "
+                    f"{match.user1.username} and {match.user2.username}."
+                )
                 self.stdout.write(self.style.SUCCESS(success_message))
 
             except Exception as e:
-                error_message = f"Failed to send notification for match between {match.user1.username} and {match.user2.username}: {e}"
+                error_message = (
+                    f"Failed to send notification for match between "
+                    f"{match.user1.username} and {match.user2.username}: {e}"
+                )
                 logger.error(error_message)
                 self.stderr.write(self.style.ERROR(error_message))
 
@@ -67,10 +72,13 @@ class Command(BaseCommand):
         message = (
             f"Hello {user.username},\n\n"
             "Exciting news! You've been matched in 'Roleplay and then Date'. "
-            "This isn't just another swipe-and-match encounter. Prepare yourself for an immersive journey of "
-            "anonymous role-playing, and a unique 28-day narrative that lets you connect with your match on a deeper level.\n\n"
-            "Log in to the app and play the game with your companion in this adventure of moonlit tales and mysterious connections. "
-            "Once inside, you can embark on your journey together and see where the story takes you.\n\n"
+            "This isn't just another swipe-and-match encounter. Prepare yourself for an "
+            "immersive journey of anonymous role-playing, and a unique 28-day narrative "
+            "that lets you connect with your match on a deeper level.\n\n"
+            "Log in to the app and play the game with your companion in this adventure "
+            "of moonlit tales and mysterious connections. "
+            "Once inside, you can embark on your journey together and see where the "
+            "story takes you.\n\n"
             "Your moonlit adventure awaits!\n\n"
             "Best wishes,\n"
             "The Roleplay and then Date Team"
