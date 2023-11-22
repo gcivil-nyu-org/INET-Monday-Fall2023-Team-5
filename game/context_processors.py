3b9f3a3f94741110eb0b1848ca0a59a9a2b897fe
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-22 09:32:23 -0500

    Update context_processors.py
    If there is an active game session and if game_session.state == Character_creation, go to the game_progress url.  Otherwise, go to character_creatioin url

--- game/context_processors.py
+++ game/context_processors.py
@@ -1,12 +1,14 @@
-from game.models import GameSession
+from game.models import GameSession, Player
 import logging
 from django.db.models import Q
+from django.urls import reverse
+
 
 
 logger = logging.getLogger(__name__)
 
 
-def game_session_processor(request):
+""" def game_session_processor(request):
     if request.user.is_authenticated:
         try:
             # Filter GameSessions where the user is either playerA or playerB
@@ -28,4 +30,28 @@ def game_session_processor(request):
             return {"game_session_url": None}
 
     # Return an empty dictionary if the user is not authenticated
+    return {} """
+
+def game_session_processor(request):
+    if request.user.is_authenticated:
+        try:
+            # Filter GameSessions where the user is either playerA or playerB
+            game_session = GameSession.objects.filter(
+                Q(playerA__user=request.user) | Q(playerB__user=request.user),
+                is_active=True
+            ).latest("id")
+
+            # Check if the player has a character
+            player = Player.objects.get(user=request.user)
+            if game_session.state == game_session.CHARACTER_CREATION:
+            #if player.character:
+                game_session_url = game_session.get_absolute_url()
+            else:
+                game_session_url = reverse("game:character_creation", kwargs={"game_id": game_session.game_id})
+
+            return {"game_session_url": game_session_url}
+
+        except (GameSession.DoesNotExist, Player.DoesNotExist):
+            return {"game_session_url": None}
+
     return {}
