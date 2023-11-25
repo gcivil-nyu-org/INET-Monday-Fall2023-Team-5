89b7cf725e0ced84b864f25d67d00a353ac7ca73
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-11-25 07:24:43 -0500

    Update forms.py

--- game/forms.py
+++ game/forms.py
@@ -44,3 +44,55 @@ class CharacterSelectionForm(forms.Form):
         empty_label=None,
         to_field_name="id",
     )
+
+class MoonSignInterpretationForm(forms.Form):
+    MOON_SIGN_CHOICES = [
+        ('positive', 'Positive'),
+        ('negative', 'Negative'),
+        ('ambiguous1', 'Ambiguous 1'),
+        ('ambiguous2', 'Ambiguous 2'),
+    ]
+
+    first_quarter = forms.ChoiceField(
+        choices=MOON_SIGN_CHOICES,
+        label="The First Quarter is a",
+        widget=forms.RadioSelect
+    )
+    first_quarter_reason = forms.CharField(
+        max_length=150,
+        label="because",
+        widget=forms.TextInput(attrs={'placeholder': 'Your reason...'})
+    )
+
+    full_moon = forms.ChoiceField(
+        choices=MOON_SIGN_CHOICES,
+        label="The Full Moon is a",
+        widget=forms.RadioSelect
+    )
+    full_moon_reason = forms.CharField(
+        max_length=150,
+        label="because",
+        widget=forms.TextInput(attrs={'placeholder': 'Your reason...'})
+    )
+
+    last_quarter = forms.ChoiceField(
+        choices=MOON_SIGN_CHOICES,
+        label="The Last Quarter is a",
+        widget=forms.RadioSelect
+    )
+    last_quarter_reason = forms.CharField(
+        max_length=150,
+        label="because",
+        widget=forms.TextInput(attrs={'placeholder': 'Your reason...'})
+    )
+
+    new_moon = forms.ChoiceField(
+        choices=MOON_SIGN_CHOICES,
+        label="The New Moon is a",
+        widget=forms.RadioSelect
+    )
+    new_moon_reason = forms.CharField(
+        max_length=150,
+        label="because",
+        widget=forms.TextInput(attrs={'placeholder': 'Your reason...'})
+    )
