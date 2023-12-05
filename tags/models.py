3c83dfbaa28e51e7b50980e870e4456c49a50d4d
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-12-05 15:04:15 -0500

    Update models.py

--- tags/models.py
+++ tags/models.py
@@ -5,5 +5,5 @@ class Tag(models.Model):
     word = models.CharField(max_length=100)
     # any other fields you might need
 
-    # def __str__(self):
-    #    return self.word
+    def __str__(self):
+        return self.word
