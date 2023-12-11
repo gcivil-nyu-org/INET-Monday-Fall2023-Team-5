602ff6ad2d773a139352ca25c75d2c3d172e274a
AndresZarta <andresnico94@hotmail.com>
2023-12-11 12:36:05 -0500

    Word bug fix

---
+++ static_dev/js/displayMysteryEmail.js
@@ -0,0 +1,8 @@
+function showEmail() {
+            var email = document.getElementById('email');
+            if(email.style.display === 'none') {
+                email.style.display = 'block';
+            } else {
+                email.style.display = 'none';
+            }
+        }
